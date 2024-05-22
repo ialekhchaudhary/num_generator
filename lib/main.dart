3728f66dc50fa920fc711cb5f58e947c6b876e0a
Alekh Chaudhary <chaudharyalekh@gmail.com>
2024-05-22 11:42:09 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:num_generator/app/app.dart';
+import 'package:num_generator/screen/gamescreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
